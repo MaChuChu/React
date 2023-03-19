4e39c5d799994e7b8d2c72e19a2103fb9b9b73e0
Ranjeth Ravichandran <ranjethravichandran@hotmail.com>
2023-03-19 15:33:47 +0000

    Started React-Portfolio

---
+++ react-portfolio-1/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+    </>
+  )
+}
+
+export default App
