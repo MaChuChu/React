69147499862004a668c0fd90a996eebfde10c8d9
Ranjeth Ravichandran <ranjethravichandran@hotmail.com>
2023-03-20 01:16:24 +0000

    Header Component (Completed)

--- react-portfolio-1/src/App.jsx
+++ react-portfolio-1/src/App.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -11,6 +17,12 @@ const App = () => {
         <Header />
         <Nav />
         <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
     </>
   )
 }
